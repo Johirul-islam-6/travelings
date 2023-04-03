80a9f1a23fd1c25977c9414cacff3dc057941f4a
Jharna Amin <jharna3khatun@gmail.com>
2023-04-03 23:18:13 +0600

    added new line in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>Get started by Travel-Xone</p>
+          <p>Get started by Travel-Xone . This is our team project</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
