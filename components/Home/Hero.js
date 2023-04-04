cd9de7049c2c46cfb959ef4df70818eca56f157a
Jharna Amin <jharna3khatun@gmail.com>
2023-04-04 11:24:08 +0600

    added home update

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -5,7 +5,8 @@ const Hero = () => {
       <div className={styles.heroBg}>
         <h1 className="text-5xl text-white">Your world of joy</h1>
         <p className="text-xl text-slate-400">
-          Find what makes you happy anytime, anywhere
+          Find what makes you happy anytime, anywhere.Find what makes you happy
+          anytime, anywhere
         </p>
       </div>
     </>
