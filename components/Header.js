472a7715f7708e7f3ed3f7a80678a73c573e630e
Md.Johirule <johirulislam574206@gmail.comm>
2023-04-06 03:05:05 +0600

    home push

--- components/Header.js
+++ components/Header.js
@@ -1,152 +1,163 @@
 import Link from "next/link";
-import { FaUserAlt } from "react-icons/fa";
+import { FaAirbnb, FaBloggerB, FaDelicious, FaDochub, FaHome, FaPhoneSquareAlt, FaUserAlt, FaUserLock } from "react-icons/fa";
 import TopHeader from "./TopHeader";
+import navbar from "../src/styles/home_style/hero.module.css"
 
 const Header = () => {
   return (
     <>
       <TopHeader />
-      <header className="navbar bg-base-100">
-        <div className="navbar-start">
-          {/* For mobile view */}
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
+      <header className="w-[100%] justify-center flex mx-auto absolute">
+        <div class="navbar w-[100%] justify-center flex mx-auto bg-[#00000021]">
+          {/* <!-- -------- toggle left small size slider ------ --> */}
+          <div class="dropdown dropdown-start md:hidden">
+            <label tabindex="1" class="btn btn-ghost btn-circle avatar">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7" />
               </svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link href="/">Home</Link>
-              </li>
 
-              <li>
-                <Link href="/destination">Destination</Link>
-              </li>
-              <li>
-                <Link href="/adventurestyles">Adventure Styles</Link>
-              </li>
-              <li>
-                <Link href="/deals">Deals</Link>
-              </li>
-              <li>
-                <Link href="/blogs">Blogs</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact Us</Link>
-              </li>
-              <li>
-                <Link href="/Register">Log In / Register</Link>
-              </li>
-            </ul>
-          </div>
+            <ul tabindex="1" class="menu menu-compact dropdown-content mt-3 bg-[#000000e7] rounded-box w-[60vw] h-[90vh]">
+              <div class="h-[100vh] p-0 mx-6  space-y-2 md:w-60 md:hidden block py-4">
+                <div class="flex justify-between w-[100%] mx-auto">
+                  <h1 className="uppercase text-[20px] font-bold text-center cursor-pointer">Tour<span className="text-[#11b3f9]">EASy</span></h1>
+                  <p className=" text-[20px]">x</p>
+                </div>
+                <div
+                  class="pt-3 before:flex before:w-full before:h-[1px] before:mb-0 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-[#fafafa] mr-3">
+                </div>
+                <div class="divide-gray-700 pt-6 relative z-50">
+                  <ul>
+                    <li class="hover:bg-[#0458c6]">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaHome />
+                        <span>Home</span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaAirbnb />
+                        <span>Adventure</span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaDelicious />
+                        <span>Destination</span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaDochub />
+                        <span>Deals</span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaBloggerB />
+                        <span>Blogs</span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <a rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaPhoneSquareAlt />
+                        <span>Contact Us </span>
+                      </a>
+                    </li>
+                    <li class="hover:bg-[#0458c6] mt-1">
+                      <Link href="/admin" rel="noopener noreferrer" href="#"
+                        class={`${navbar.link_style_sm} flex items-center p-2 space-x-3 rounded-md`}>
+                        <FaUserLock />
 
-          {/* For Desktop view */}
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/destination">Destination</Link>
-            </li>
-            <li>
-              <Link href="/adventurestyles">Adventure Styles</Link>
-            </li>
-            <li>
-              <Link href="/deals">Deals</Link>
-            </li>
-            <li>
-              <Link href="/blogs">Blogs</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link href="/register">Log In / Register</Link>
-            </li>
-          </ul>
-        </div>
+                        <p className="ml-2 text-sm text-[#f7f7f7] font-bold text-[13px] bg-[#0b4bee] px-3 py-1">Admin </p>
 
-        {/* end menu */}
-        <div className="navbar-end">
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-sm indicator-item">8</span>
-              </div>
-            </label>
-            <div
-              tabIndex={0}
-              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-            >
-              <div className="card-body">
-                <span className="font-bold text-lg">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn btn-primary btn-block">
-                    View cart
-                  </button>
+                      </Link>
+                    </li>
+
+                  </ul>
+
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-8 rounded-full">
-                <FaUserAlt className="w-8 h-8 text-blue-700 bg-base-300 p-2" />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
             </ul>
           </div>
-        </div>
-      </header>
+
+          {/* <!-- -----only small size navbar satar----- --> */}
+          <div class="flex-1 flex justify-start w-[100%] reletive md:hidden navbar-center pl-2">
+            <h1 className="uppercase text-[25px] font-bold text-center cursor-pointer">Tour<span className="text-[#11b3f9]">EASy</span></h1>
+          </div>
+
+          <div class="flex-1 flex justify-end w-full  reletive md:hidden navbar-end">
+
+            <button className="h-[33px] text-[13px] px-4 text-[#dfdfdf] bg-[#0272f2] rounded-md font-bold hover:bg-[#2d89f1da]"> Login</button>
+
+
+            <div class="search_btn bg-slate-100 w-10 h-8 rounded-md ml-2">
+              <img class="w-full h-full rounded" src="https://i.ibb.co/74dCMRZ/profile-baground22.png"
+                alt="Default avatar" />
+            </div>
+
+
+          </div>
+          {/* -----------only small size navbar end----------- */}
+
+          {/* ===================================== only larze size show ======================= */}
+          <ul className="align-middle hidden md:flex justify-center w-full">
+            <div class="navbar-start ">
+              <h1 className="uppercase text-[30px] font-bold cursor-pointer">Tour<span className="text-[#11b3f9]">EASy</span></h1>
+            </div>
+
+            <div class="navbar-cente flex justify-center">
+              <ul className="menu menu-horizontal px-1">
+                <li className=""><Link href={''} className={`${navbar.link_style}`}>Home</Link></li>
+                <li><Link href={''} className={`${navbar.link_style}`}>Destination</Link></li>
+                <li><Link href={''} className={`${navbar.link_style}`}>Adventure</Link></li>
+                <li><Link href={''} className={`${navbar.link_style}`}>Contact</Link></li>
+                <li><Link href={''} className={`${navbar.link_style}`}>Blogs</Link></li>
+                <li><Link href={''} className={`${navbar.link_style}`}>Contact Us  </Link></li>
+                <li tabIndex={0}>
+                  <Link href={''} className={`${navbar.link_style}`} >
+                    countries
+                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                  </Link>
+                  <ul className="p-2 bg-base-100">
+                    <li><Link href={''} className={`${navbar.link_style}`}>Bangladesh</Link></li>
+                    <li><Link href={''} className={`${navbar.link_style}`}>India</Link></li>
+                  </ul>
+                </li>
+
+              </ul>
+            </div>
+            <div className="navbar-end flex justify-center items-center">
+              <ul class="menu menu-horizontal px-1 flex justify-center items-center">
+
+                <button className="h-[40px] px-8 text-[#dfdfdf] bg-[#0272f2] rounded-md font-bold hover:bg-[#2d89f1da]"> Login</button>
+
+                <li class="">
+                  <a class="">
+                    <div class="avatar w-[45px] h-[45px rounded">
+                      <div class="w-[100%] rounded">
+                        <img src="https://media.licdn.com/dms/image/D5635AQEgyJhAVazUdw/profile-framedphoto-shrink_400_400/0/1678483166503?e=1680818400&v=beta&t=GJTU4zuWm7v8L66LbKvlUdhcbyu8XOGU_BAqVtzo798"
+                          class="p-[3px]" alt="admin" />
+                      </div>
+                    </div>
+                  </a>
+
+                </li>
+
+
+              </ul>
+            </div>
+          </ul>
+        </div >
+      </header >
     </>
   );
 };
--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,14 +1,175 @@
-import styles from "../../src/styles/#hero.module.css";
+import styles from "../../src/styles/home_style/hero.module.css";
+import { FaCalendarAlt, FaChild, FaClipboardCheck, FaCrown, FaFighterJet, FaHandHoldingUsd, FaPeriscope, FaSplotch, FaUserAlt, FaWalking, IconName } from "react-icons/fa";
 const Hero = () => {
   return (
     <>
-      <div className={styles.heroBg}>
-        <h1 className="text-5xl text-white">Your world of joy</h1>
-        <p className="text-xl text-slate-400">
-          Find what makes you happy anytime, anywhere.Find what makes you happy
-          anytime, anywhere
-        </p>
-      </div>
+      <section className="Section01">
+        <div className={`${styles.heroBg} flex justify-center items-center md:h-[95vh] h-[120vh]`}>
+          <div className="container">
+            <h1 className="md:text-5xl text-4xl text-[#ede7e7] pt-10 md:pt-2 font-bold ">Your World of Joy</h1>
+            <p className="text-xl text-slate-400 pt-3 px-[2px]">
+              Find what makes you happy anytime, anywhere. Find what makes  you happy
+              anytime, anywhere
+            </p>
+            <div className="card_container h-[auto] border-[0.5px] border-[#0293cc] py-6  bg-[#00000076] text-white mx-auto w-[80%] gap-y-2 mt-14 rounded-3xl grid grid-cols-1 md:grid-cols-5 justify-around items-center">
+              {/* --------Home page middle part card inside design ---------------- */}
+              <div className="dropdown dropdown-bottom cursor-pointer dropdown-end md:mx-5">
+                <div tabIndex={0} className="inside flex justify-center items-center">
+                  <div className="icons p-2 bg-[#d5d0d0] rounded-md">
+                    <FaPeriscope className="text-black" />
+                  </div>
+                  <div className="content ml-3">
+
+                    <h1 tabIndex={0} className="text-start font-bold">Location</h1>
+                    <p tabIndex={0} className=" text-[12px]">Where you are going?</p>
+
+                  </div>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-[#000000b0] rounded-box w-52 text-white">
+                  <li><a>Dhaka, Bangladesh</a></li>
+                  <li><a>kolkata, India</a></li>
+                </ul>
+              </div>
+              {/* 02 card */}
+              <div className="dropdown dropdown-bottom cursor-pointer dropdown-end md:mx-5 md:flex">
+                <div
+                  class="hidden md:block before:flex before:w-[3px] before:h-[50px] before:mb-0 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-[#fafafacf] mr-3 ">
+                </div>
+                <div tabIndex={0} className="inside flex justify-center items-center mr-11">
+                  <div className="icons p-2 bg-[#d5d0d0] rounded-md">
+                    <FaCalendarAlt className="text-black" />
+                  </div>
+                  <div className="content ml-3">
+
+                    <h1 tabIndex={0} className="text-start font-bold">Check In </h1>
+                    <p tabIndex={0} className=" text-[12px] text-start">Add Date</p>
+
+                  </div>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-[#000000b0] rounded-box w-52 text-white">
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 2</a></li>
+                </ul>
+              </div>
+              {/* 03 card */}
+              <div className="dropdown dropdown-bottom cursor-pointer dropdown-end mx-5 md:flex">
+                <div
+                  class="hidden md:block before:flex before:w-[3px] before:h-[50px] before:mb-0 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-[#fafafacf] mr-3">
+                </div>
+                <div tabIndex={0} className="inside flex justify-center items-center mr-8">
+                  <div className="icons p-2 bg-[#d5d0d0] rounded-md">
+                    <FaCalendarAlt className="text-black" />
+                  </div>
+                  <div className="content ml-3">
+
+                    <h1 tabIndex={0} className="text-start font-bold">Check Out </h1>
+                    <p tabIndex={0} className="text-start text-[12px]">out Date</p>
+
+                  </div>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-[#000000b0] rounded-box w-52 text-white">
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 2</a></li>
+                </ul>
+              </div>
+              {/* 04 card */}
+              <div className="dropdown dropdown-bottom cursor-pointer dropdown-end mx-5 md:flex">
+                <div
+                  class="hidden md:block before:flex before:w-[3px] before:h-[50px] before:mb-0 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-[#fafafacf] mr-3">
+                </div>
+                <div tabIndex={0} className="inside flex justify-center items-center mr-16">
+                  <div className="icons p-2 bg-[#d5d0d0] rounded-md">
+                    <FaUserAlt className="text-black" />
+                  </div>
+                  <div className="content ml-3">
+
+                    <h1 tabIndex={0} className="text-start font-bold">Guest </h1>
+                    <p tabIndex={0} className=" text-[14px] text-start">45</p>
+
+                  </div>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-[#000000b0] rounded-box w-52 text-white">
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 2</a></li>
+                </ul>
+              </div>
+              {/* 05 Search */}
+              <div className="dropdown dropdown-bottom cursor-pointer dropdown-end mx-5 md:flex">
+                <div
+                  class=" hidden md:block before:flex before:w-[3px] before:h-[50px] before:mb-0 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-[#fafafacf] mr-3">
+                </div>
+                <div className="inside flex justify-center md:justify-start text-center items-center">
+                  <div class="relative z-0">
+                    <div
+                      class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                      </svg>
+                    </div>
+                    <input type="search" id="search"
+                      class="block w-full p-3 pl-10 text-sm border border-gray-300 rounded-lg bg-gray-5 dark:bg-[#00000029] dark:placeholder-gray-400 dark:text-white text-start"
+                      placeholder="Search enithing..." required />
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* ------------- Card Section down home------------ */}
+      <section className="section01_card flex justify-center items-center w-[100%] py-10">
+        <div className="grid md:grid-cols-4 grid-cols-1 gap-4 justify-center w-[70%]">
+
+          <div className={`${styles.section02} card w-full py-2 px-3 border-t-4  border-[#0475d8]`}>
+            <div className="icon_box flex justify-center items-center py-3 px-3 w-[34%] mx-auto bg-[#0475d8] rounded-full">
+              <FaWalking className="text-[50px]" />
+            </div>
+            <div className="card-body text-black">
+              <h2 className="text-center text-[23px] font-semibold">3500+</h2>
+              <p className="font-bold text-[#000000b0] text-[16px]">Happy Travelars</p>
+            </div>
+          </div>
+
+
+          <div className={`${styles.section02} card w-full pt-3 px-3 md:border-b-4 border-t-4 border-[#0475d8]`}>
+            <div className="icon_box flex justify-center items-center py-3 px-3 w-[34%] mx-auto bg-[#0475d8] rounded-full">
+              <FaChild className="text-[50px]" />
+            </div>
+            <div className="card-body text-black">
+              <h2 className="text-center text-[23px] font-semibold">495+</h2>
+              <p className="font-bold text-[#000000b0] text-[16px]">Tours Success</p>
+            </div>
+          </div>
+
+          <div className={`${styles.section02} card w-full pt-3 px-3 border-t-4 border-[#0475d8]`}>
+            <div className="icon_box flex justify-center items-center py-3 px-3 w-[34%] mx-auto bg-[#0475d8] rounded-full">
+              <FaClipboardCheck className="text-[50px]" />
+            </div>
+            <div className="card-body text-black">
+              <h2 className="text-center text-[23px] font-semibold">99.5%+</h2>
+              <p className="font-bold text-[#000000b0] text-[16px]">Positive Reviews</p>
+            </div>
+          </div>
+
+          <div className={`${styles.section02} card w-full pt-3 px-3 border-t-4 md:border-b-4 border-[#0475d8]`}>
+            <div className="icon_box flex justify-center items-center py-3 px-3 w-[34%] mx-auto bg-[#0475d8] rounded-full">
+              <FaCrown className="text-[50px]" />
+            </div>
+            <div className="card-body text-black">
+              <h2 className="text-center text-[23px] font-semibold">203+</h2>
+              <p className="font-bold text-[#000000b0] text-[16px]">Award Wining</p>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </>
   );
 };
